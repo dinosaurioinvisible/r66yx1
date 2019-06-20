ce31645a2cf04511c34621ad2665a72466e2c043
ferv <ferodriguezv@gmail.com>
2019-06-20 15:25:50 -0300

    v4?

--- agent.py
+++ agent.py
@@ -4,21 +4,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
+import copy
 import pdb
 
 # agent
 class Agent:
-    def __init__(self, x=-2.5, nodes=[], kw=0.0025, kd=1000, kt=1):
-        self.x = x
-        self.nodes = nodes
+    def __init__(self, sim_time=15\
+    ,x=[], xmax=[]
+    ,kw=0.0025, kd=1000, kt=1, nodes=[]):
+        self.sim_time = sim_time
+        self.x = np.array(x)
+        self.xmax = xmax
         self.kw = kw
         self.kd = kd
-        self.kt = 1
+        self.kt = kt
+        self.nodes = nodes
+        self.sensor_hist = []
         ####
-        # t=0, velociy=1 for convinience so attraction isn't nan
-        # self.nodes.append([self.x, 0, 0)
+        # initial node
+        # self.nodes.append([self.x, 0, 0])
+        # 
+        #
+        #
         # records for position (independent of the creation of nodes)
-        self.history = []
+        self.all_x = []
+        self.all_nodes = []
+        self.why = []
 
     # escaling fx
     # Np & Nv are calculated in a normalized SM space where
@@ -31,17 +42,20 @@ class Agent:
     # weight function
     # weight_fx = 2/1+np.exp(-kw*Nw)
     def weight_fx(self, Nw):
-        weight = 2/1+np.exp(-self.kw*Nw)
+        weight = 2/(1+np.exp(-self.kw*Nw))
         return weight
 
     # distance function
     # distance_fx = 2/1+np.exp(kd*(Np-x)**2)
     def distance_fx(self, x, Np):
         # scale values
-        xmax = max([node[0] for node in self.nodes])
-        ex = self.scale(x, xmax)
-        eNp = self.scale(Np, xmax)
-        distance = 2/(1+np.exp(self.kd*((ex-eNp)**2)))
+        # scale from dataset values (problem : values change : scaling is incorrect)
+        # xmax = max([max(np.absolute(node[0])) for node in self.nodes])
+        u = self.scale(x, self.xmax)
+        v = self.scale(Np, self.xmax)
+        # euclidian distance between vectors
+        eudist = np.sqrt(sum([(i-j)**2 for i,j in zip(u,v)]))
+        distance = 2/(1+np.exp(self.kd*((eudist)**2)))
         return distance
 
     # density function
@@ -49,8 +63,8 @@ class Agent:
     def density_fx(self):
         density = 0
         for node in self.nodes:
-            Nw = np.array(node[2])
-            Np = np.array([node[0]])
+            Nw = node[2]
+            Np = np.array(node[0])
             weight = self.weight_fx(Nw)
             distance = self.distance_fx(self.x, Np)
             density += weight*distance
@@ -60,9 +74,15 @@ class Agent:
     # attraction_fx = (Np - x) - (Np-x)*(Nv/np.absolute(Nv))
     def attraction_fx(self, x, Np, Nv):
         # scale
-        vmax = max([node[1] for node in self.nodes])
-        nNv = self.scale(Nv, vmax)
-        attraction = (Np-x)-(Np-x)*(Nv/nNv)
+        # vmax = max(np.absolute([node[1] for node in self.nodes]))
+        # if-else just not to divide by zero
+        # nNv = self.scale(Nv, vmax) if Nv else 1
+        #
+        #
+        # if-else just not to divide by zero
+        nNv = np.sqrt(Nv**2)
+        nNv=1 if not nNv else nNv
+        attraction = (Np-x)-((Np-x)*(Nv/nNv))
         return attraction
 
     # IDSM motor influence function
@@ -71,13 +91,14 @@ class Agent:
         density = self.density_fx()
         motor_influence = 0
         for node in self.nodes:
-            Np = np.array([node[0]])
-            Nv = np.array([node[1]])
-            Nw = np.array(node[2])
+            Np = np.array(node[0])
+            Nv = np.array(node[1])
+            Nw = node[2]
             weight = self.weight_fx(Nw)
             distance = self.distance_fx(self.x, Np)
             # velocity : Nv
             attraction = self.attraction_fx(self.x, Np, Nv)
+            #
             motor_influence += weight*distance*(Nv+attraction)
         idsm_influence = (1/density)*motor_influence
         # new position
@@ -88,12 +109,18 @@ class Agent:
             # velocity is given by the idsm
             # position = x + idsm influence
             # initial weight = 0
-            self.nodes.append([self.x, idsm_influence, 0])
-            print("node added")
-            print([self.x, idsm_influence, 0])
-        else:
-            print("nothing")
-        print("density = "+str(density))
+            self.nodes.append([copy.deepcopy(self.x), idsm_influence, 0])
+        #
+        #
+        #
+        self.all_nodes.append([copy.deepcopy(self.x), idsm_influence, 0])
+        self.why.append([density, motor_influence, Nv, attraction, idsm_influence])
+
+    # sensory input
+    def sensory_fx(self):
+        # s = 1 / (1 + x**2)
+        s_input = 1/(1+self.x**2)
+        self.sensor_hist.append(s_input)
 
     # node's reinforcement function
     # r(N,x) = 10 * d(Np,x)
@@ -103,56 +130,45 @@ class Agent:
         return reinforcement
 
     # maintenance of nodes (updating of nodes' weights)
-    # dNw/dt = -1 + r(N,x)
     def maintenance_fx(self):
         for n in range(len(self.nodes)):
             Np = np.array(self.nodes[n][0])
             reinforcement = self.reinforcement_fx(self.x, Np)
-            updated_weight = -1+reinforcement
+            # dNw/dt = -1 + r(N,x)
+            updated_weight = -1 + reinforcement
+            # = -1+r | += -1+r
             self.nodes[n][2] = updated_weight
 
-    # training
-    # for the first 20 time units the robot is controlled by a training controller
-    def training_fx(self, t=20):
-        for t in range(1, t+1):
-            # movement
-            velocity = np.cos(t/2)/2
-            weight = 0
-            self.x += velocity
-            # creation of nodes
-            self.history.append(self.x)
-            self.nodes.append([self.x, velocity, weight])
-        plt.plot([t for t in range(len(self.nodes))], [i[0] for i in self.nodes])
-        plt.scatter([t for t in range(len(self.nodes))], [i[0] for i in self.nodes])
-        plt.xlabel("Time")
-        plt.ylabel("Position")
-        #plt.savefig("TrainingPlot")
+    #####################################################
+                      # Example experiments
+    #####################################################
 
-    # idsm control phase
-    def operation(self, time=10):
-        print("\n")
-        [print(node) for node in self.nodes]
-        print("\n")
-        for t in range(time):
-            print("\n")
-            print(t)
-            self.idsm_fx()
-            self.maintenance_fx()
-            [print(node) for node in self.nodes[-5:-1]]
-            self.history.append(self.x)
-        plt.plot([t for t in range(len(self.nodes))], [i[0] for i in self.nodes])
-        plt.scatter([t for t in range(len(self.nodes))], [i[0] for i in self.nodes])
-        plt.savefig("WholePlot")
-        plt.close()
-        # print positions independently of the creation of the nodes
-        plt.plot([t for t in range(len(self.history))], [i for i in self.history])
-        plt.savefig("AgentHistory")
-        plt.close()
-
-
-
-    # experiment
-
-    # __main__
-    # if __name__ == "__main__":
-    #    main()
+    # Figure 3
+    # 2-motor, 0-sensor IDSM
+    # sim_time = 20
+    # manually added 4 nodes in relative proximity
+    # externally assigned 2 motor states
+    # m1 = 0.75 * cos( (2π / 10) * t
+    # m2 = 0.75 * sin( (2π / 10) * t
+    def figure3(self, time=20, iw = [-500, -100, 0, 50, 100]):
+        for iwx in iw:
+            # nodes = [position, velocity, weight]
+            n1 = [[0.55, 0.50], 0, iwx]
+            n2 = [[0.50, 0.45], 0, 0]
+            n3 = [[0.45, 0.50], 0, 0]
+            n4 = [[0.50, 0.55], 0, 0]
+            self.nodes.append(n1, n2, n3, n4)
+            for t in range(1, time+1):
+                self.idsm_fx()
+                self.maintenance_fx()
+                self.all_x.append(copy.deepcopy(self.x))
+            # plot x, y
+            plt.plot([node[0][0] for node in self.nodes],[node[0][1] for node in self.nodes])
+            figname = "node_weight_fig3_{}".format(iwx)
+            plt.savefig(figname)
+            plt.close
+            # plot weight influence
+            # plt.streamplot(x, y, u, v)
+            # figname = "influence_fig3_{}".format(iwx)
+            # plt.savefig(figname)
+            # plt.close
