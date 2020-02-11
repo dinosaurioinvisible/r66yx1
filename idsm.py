c3f2dd51bcdd37606749b4f5f38a5e989e45ded6
Fernando Rodríguez Vergara <ferodriguezv@gmail.com>
2020-02-11 19:59:24 +0000

    idsm working for the 1d example

--- idsm.py
+++ idsm.py
@@ -23,25 +23,35 @@ class Agent:
         # initial sm state
         self.nodes = []
         self.x = np.concatenate((np.array([0]*sdim), np.array([0]*mdim)))
+        self.c = 0
+        self.amp_factor = 1
 
     # for each timestep
     def idsm(self, sx):
         self.update_nodes()
-        # compute movement from idsm
-        mx = self.motor_fx(sx)
+        idsm_mu = self.motor_fx()
+        mx = self.x[self.sdim:] + idsm_mu * self.amp_factor
+        # mx = (1+self.x[self.sdim:])/2 + idsm_mu * self.amp_factor
         self.velocity = np.concatenate((sx,mx)) - self.x
         self.add_node()
-        if sx < 0 or sx > 1:
-            import pdb; pdb.set_trace()
         self.x = np.concatenate((sx,mx))
+        ##
+        self.c += 1
+        if self.c <= 10:
+            print("idsm")
+            print(self.c)
+            print(idsm_mu)
+            print(mx)
+            print(self.x)
+            print(self.velocity)
+        # import pdb; pdb.set_trace()
+        ##
         return mx
 
     def train(self, sx, mx):
         self.update_nodes()
         # normalize mx to fit the sm-space [0,1], [sx(-5)=0.03, sx(0)=1]
         mx = (1+mx)/2
-        if mx > 1:
-            import pdb; pdb.set_trace()
         self.velocity = np.concatenate((sx,mx)) - self.x
         self.add_node()
         self.x = np.concatenate((sx,mx))
@@ -54,6 +64,17 @@ class Agent:
             self.nodes.append(Node(self.x, self.velocity))
             # print("node added, pos={}, vel={}".format(self.x, self.velocity))
 
+    # for debugging
+    def node_map(self):
+        active_nodes = []
+        for node in self.nodes:
+            if node.active:
+                active_nodes.append([node.position, node.velocity, node.weight])
+        if len(active_nodes) > 1:
+            for n in enumerate(active_nodes):
+                print("{}: {} - {} - {}".format(n[0],n[1][0],n[1][1],n[1][2]))
+            import pdb; pdb.set_trace()
+
     # density fx
     def density_fx(self):
         density = 0
@@ -62,9 +83,9 @@ class Agent:
                 weight = self.weight_fx(node.weight)
                 distance = self.distance_fx(node.position)
                 density += weight*distance
-        if density == 0:
+        # if density == 0:
             # import pdb; pdb.set_trace()
-            print("density=0, number of nodes: {}".format(len(self.nodes)))
+            # print("density=0, number of nodes: {}".format(len(self.nodes)))
         return density
     # weight fx
     def weight_fx(self, Nw):
@@ -78,10 +99,10 @@ class Agent:
         return weight
     #distance fx
     def distance_fx(self, Np):
-        node_dist = np.linalg.norm(Np-self.x)/10
+        node_dist = np.linalg.norm(Np-self.x)
         if node_dist > 0.15:
-            print("node dist: {}".format(node_dist))
-            import pdb; pdb.set_trace()
+            # print("node dist: {}".format(node_dist))
+            # import pdb; pdb.set_trace()
             return 0
         distance = 2/(1+np.exp(self.kd*node_dist**2))
         return distance
@@ -95,14 +116,12 @@ class Agent:
             node.activation_fx(self.dt)
 
     # motor responses
-    def motor_fx(self, sx):
+    def motor_fx(self):
         ix = self.influence_fx()
         dx = self.density_fx()
-        if ix <= 0:
-            import pdb; pdb.set_trace()
-        dmu = (ix/dx)*self.dt
-        #print(dmu)
-        #import pdb; pdb.set_trace()
+        if dx == 0:
+            return 0
+        dmu = (ix/dx)#*self.dt
         return dmu
     # velocity + attraction
     def influence_fx(self):
@@ -119,19 +138,23 @@ class Agent:
                 # velocity component
                 velocity = node.velocity[self.sdim:]
                 # attraction component
-                a = node.position[self.sdim:] - self.x[self.sdim:]
-                normed_vel = velocity/np.linalg.norm(velocity)
-                attraction = a - a*normed_vel
+                att = node.position[self.sdim:] - self.x[self.sdim:]
+                normed_vel = np.linalg.norm(velocity)
+                if normed_vel == 0:
+                    attraction = att
+                else:
+                    attraction = att - att*normed_vel
                 # weighted motor influence
-                influence += (weight*distance*(velocity+attraction))*self.dt
+                influence += (weight*distance*(velocity+attraction))
                 nodes += 1
                 sweights += weight
                 sdistances += distance
                 svelocities += velocity
                 sattractions += attraction
-        print(self.x)
-        print("{},{},{},{},{},{}".format(nodes,sweights,sdistances,svelocities,sattractions,influence))
-        # import pdb; pdb.set_trace()
+                # import pdb; pdb.set_trace()
+        #print(self.x)
+        #print("{},{},{},{},{},{}".format(nodes,sweights,sdistances,svelocities,sattractions,influence))
+        #import pdb; pdb.set_trace()
         return influence
 
 
--- idsm_sim.py
+++ idsm_sim.py
@@ -6,37 +6,39 @@ import matplotlib.animation as animation
 import matplotlib.gridspec as gridspec
 
 
-def idsm_simulation(tx=0, dt=0.1, t=50):
+def idsm_simulation(tx=0, dt=0.1, t=65, map=False):
     simdata = []
     robot = idsm.Agent(dt)
     loc = np.array([-2.5])
+    sx = 0
     # training
     while tx < 20:
-        mx = np.array([np.cos(tx/2)/2])
         sx = 1/(1+loc**2)
+        mx = np.array([np.cos(tx/2)/2])
         robot.train(sx, mx)
         loc += mx*dt
         tx += dt
         simdata.append(np.concatenate((sx,loc)))
-        #print("tx:{} - sx:{} - mx:{}, loc:{}".format(tx,sx,mx,loc))
+        # print("tx:{} - sx:{} - mx:{}, loc:{}".format(tx,sx,mx,loc))
+
     # mapping of motor influence
     influence_data = []
-    for motor in np.arange(0.02,0.98,0.02):
-        for sensor in np.arange(0.02,0.98,0.02):
-            sm = np.array([motor, sensor])
-            mu = robot.motor_fx(sm)
-            xt = np.sqrt((1/sensor)-1)
-            nmotor = motor + mu[0]*0.1
-            xt = xt + 0.1 * (nmotor+1)/2
-            nsensor = 1/(1+xt**2)
-            influence_data.append([sensor,motor,nsensor-sensor,nmotor-motor])
-            # print("sensor:{}, motor:{}, nsen-sen:{}, nmotor-motor:{}".format(sensor,motor,nsensor-sensor,nmotor-motor))
+    if map:
+        for motor in np.arange(0.02,0.98,0.02):
+            for sensor in np.arange(0.02,0.98,0.02):
+                sm = np.array([motor, sensor])
+                mu = robot.motor_fx(sm)
+                xt = np.sqrt((1/sensor)-1)
+                nmotor = motor + mu[0]*0.1
+                xt = xt + 0.1 * (nmotor+1)/2
+                nsensor = 1/(1+xt**2)
+                influence_data.append([sensor,motor,nsensor-sensor,nmotor-motor])
+                # print("sensor:{}, motor:{}, nsen-sen:{}, nmotor-motor:{}".format(sensor,motor,nsensor-sensor,nmotor-motor))
     # idsm
-    while tx < 35:
-        mx = robot.idsm(sx)
+    while tx < t:
         sx = 1/(1+loc**2)
-        # loc = (1+loc)/2 + mx*amp_factor
-        loc += mx
+        mx = robot.idsm(sx)
+        loc += ((mx*2)-1)*dt
         tx += dt
         simdata.append(np.concatenate((sx,loc)))
         # print("tx:{} - sx:{} - mx:{}, loc:{}".format(tx,sx,mx,loc))
@@ -44,9 +46,9 @@ def idsm_simulation(tx=0, dt=0.1, t=50):
     # idsm rellocated
     loc = np.array([-2.5])
     while tx < t:
-        mx = robot.idsm(sx)
         sx = 1/(1+loc**2)
-        loc += mx
+        mx = robot.idsm(sx)
+        loc += ((mx*2)-1)*dt
         tx += dt
         simdata.append(np.concatenate((sx,loc)))
         #print("tx:{} - sx:{} - mx:{}, loc:{}".format(tx,sx,mx,loc))
@@ -55,6 +57,8 @@ def idsm_simulation(tx=0, dt=0.1, t=50):
 
 
 def idsm_streamplot(influence_data):
+    if len(influence_data) == 0:
+        return None
     Y,X = np.mgrid[0.02:0.98:48j, 0.02:0.98:48j]
     U,V = np.mgrid[0.0:0.0:48j, 0.0:0.0:48j]
     # import pdb; pdb.set_trace()
@@ -75,17 +79,17 @@ def idsm_streamplot(influence_data):
 
 
 def idsm_animation(simdata):
-    fig = plt.figure()
-    ax = plt.axes(xlim=(0,len(simdata)), ylim=(-5,0))
     robot_sensors = [data[0] for data in simdata]
     robot_locs = [data[1] for data in simdata]
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0,len(simdata)), ylim=(min(robot_locs),max(robot_locs)))
     robot = plt.Circle((0, robot_locs[0]), radius=0.1, color="blue", fill=True)
     xlocs, ylocs = [], []
     past_locations, = plt.plot([], [], color="grey")
 
     def init():
-        xi = [0, 20, 35]
-        yi = [robot_locs[0], robot_locs[20], robot_locs[35]]
+        xi = [0, 20, 35, len(robot_locs)]
+        yi = [robot_locs[0], robot_locs[20], robot_locs[35], robot_locs[-1]]
         ax.scatter(xi, yi, color="grey")
         ax.add_patch(robot)
         return robot,
