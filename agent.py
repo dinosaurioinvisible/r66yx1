0342e14b0a342ebfeaaff062e8393795da1baa69
Fernando Rodríguez Vergara <ferodriguezv@gmail.com>
2020-02-26 18:25:20 +0000

    fixing issues in sensors, now working. Still I'd like to check once more on communication channel

--- agent.py
+++ agent.py
@@ -1,9 +1,13 @@
 
 import numpy as np
 import geometry
+from shapely.geometry import Point
+from shapely.geometry import LineString
+from shapely.geometry.polygon import Polygon
 import sensors
 import evol_net
 import tree
+import world
 # import motor
 # import body
 
@@ -22,9 +26,9 @@ import tree
 
 class Agent:
     def __init__(self, x=100, y=100, o=0\
-    , energy=100\
+    , energy=500\
     , r=2.5, max_speed=5\
-    , feed_range=5, feed_rate=5):
+    , feed_range=10, feed_rate=5):
         # init
         self.energy = energy
         self.x = x; self.y = y; self.o = o
@@ -54,10 +58,11 @@ class Agent:
         # controller
         lw, rw, com = self.net.action(self.state)
         self.communicate(com)
-        self.move(lw, rw)
-        self.feed(objects)
+        self.move(lw, rw, objects)
+        if self.state[2] > 0:
+            self.feed(objects)
 
-    def move(self, lw, rw):
+    def move(self, lw, rw, objects):
         # update x, y, or
         lw = lw*self.max_speed
         rw = rw*self.max_speed
@@ -69,15 +74,35 @@ class Agent:
         if self.energy > 0:
             self.x += dx
             self.y += dy
-            self.o += do
-            self.o = geometry.force_angle(self.o)
+            self.o = geometry.force_angle(self.o+do)
             self.energy -= 1
+            self.bouncing_fx(dx, dy, objects)
+
+    def bouncing_fx(self, dx, dy, objects):
+        # so objects don't traspass each other
+        pos = Point(self.x, self.y)
+        body = pos.buffer(self.r)
+        contact = False
+        for object in objects:
+            if type(object) == world.Wall:
+                wall = LineString([(object.xmin,object.ymin),(object.xmax,object.ymax)])
+                if body.intersects(wall):
+                    contact = True
+            else:
+                obj_center = Point(object.x, object.y)
+                obj_body = obj_center.buffer(object.r)
+                if body.intersects(obj_body):
+                    contact = True
+        if contact:
+            self.x -= dx*2
+            self.y -= dy*2
+            self.energy -= 2
 
     def feed(self, objects):
         # feed if tree is near
         trees = [x for x in objects if type(x)==tree.Tree]
         for tx in trees:
-            dist = np.linalg.norm(np.array([tx.x,tx.y])-np.array([self.x,self.y]))
+            dist = 1 #self.r + tx.r + np.linalg.norm(np.array([tx.x,tx.y])-np.array([self.x,self.y]))
             if dist <= self.feed_range:
                 feed_rate = self.feed_rate*(1/dist)
                 e = tx.feeding_fx(feed_rate)
--- world_animation.py
+++ world_animation.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Wedge
 import matplotlib.animation as animation
+import simulation
 
 # agent ps = [ir_range, ir_angles, olf_range, olf_angles, aud_range, aud_angles]
 # agent states = [ir1, ir2, olf, aud1, aud2, e]
@@ -14,6 +15,7 @@ def sim_animation(limits, walls, trees, agents, past=True, start=True):
     # defs
     fig = plt.figure()
     ax = plt.axes(xlim=(0,limits[0]), ylim=(0,limits[1]), aspect="equal")
+    ax.grid = True
     # plot basics for agents
     agent_objs = []
     olf_domain = []
@@ -22,15 +24,15 @@ def sim_animation(limits, walls, trees, agents, past=True, start=True):
     # agent objects
     for agent in agents:
         # create objects
-        agent_obj = plt.Circle((agent.positions[0][0],agent.positions[0][1]), radius=agent.r, color="blue", fill=True)
-        agent_objs.append(agent_obj)
-        olf = Wedge((0,0), r=agent.ps[2], theta1=0, theta2=0, color="purple", fill=False, visible=False)
+        ag = plt.Circle((agent.positions[0][0],agent.positions[0][1]), radius=agent.r, color="blue", fill=True)
+        agent_objs.append(ag)
+        olf = Wedge((0,0), r=agent.ps[2], theta1=0, theta2=0, color="purple", fill=False, visible=True)
         olf_domain.append(olf)
         aud_left = plt.Circle((0,0), radius=agent.ps[4], color="red", fill=False, visible=False)
         aud_right = plt.Circle((0,0), radius=agent.ps[4], color="red", fill=False, visible=False)
         aud_domain.append([aud_left, aud_right])
-        ir_left = Wedge((0,0), r=agent.ps[0], theta1=0, theta2=0, color="orange", fill=False, visible=False)
-        ir_right = Wedge((0,0), r=agent.ps[0], theta1=0, theta2=0, color="orange", fill=False, visible=False)
+        ir_left = Wedge((0,0), r=agent.ps[0], theta1=0, theta2=0, color="orange", fill=False, visible=True)
+        ir_right = Wedge((0,0), r=agent.ps[0], theta1=0, theta2=0, color="orange", fill=False, visible=True)
         vis_domain.append([ir_left, ir_right])
 
     def init():
@@ -63,6 +65,7 @@ def sim_animation(limits, walls, trees, agents, past=True, start=True):
             y = agents[enum].positions[i][1]
             # agent (circle)
             agent_obj.center = (x, y)
+
             # olf domain (wedge)
             olf_x = x + agents[enum].r*np.cos(np.radians(o))
             olf_y = y + agents[enum].r*np.sin(np.radians(o))
@@ -72,12 +75,15 @@ def sim_animation(limits, walls, trees, agents, past=True, start=True):
             olf_domain[enum].theta1 = olf_o1
             olf_domain[enum].theta2 = olf_o2
             olf_domain[enum]._recompute_path()
-            # signals
-            #olf_domain[enum].set_visible = False
+            # olf signals
             olf_val = agents[enum].states[i][2]
             if olf_val > 0:
-                print(olf_val)
-                olf_domain[enum].visible = True
+                olf_domain[enum].set_visible(True)
+                # olf_domain[enum].fill = True
+            else:
+                olf_domain[enum].set_visible(False)
+                olf_domain[enum].fill = False
+
             # aud domain (circles)
             for n in range(len(aud_domain[enum])):
                 aud_o = o + agents[enum].ps[5][n]
@@ -85,11 +91,15 @@ def sim_animation(limits, walls, trees, agents, past=True, start=True):
                 aud_y = y + agents[enum].r*np.sin(aud_o)
                 aud_domain[enum][n].center = (aud_x, aud_y)
                 # signal
-                #aud_domain[enum][n].set_visible = False
                 aud_val = agents[enum].states[i][3+n]
                 if aud_val > 0:
+                    print("aud")
                     print(aud_val)
-                    aud_domain[enum][n].set_visible = True
+                    aud_domain[enum][n].set_visible(True)
+                    import pdb; pdb.set_trace()
+                else:
+                    aud_domain[enum][n].set_visible(False)
+
             # vis domain (wedges)
             for n in range(len(vis_domain[enum])):
                 vis_o = geometry.force_angle(agents[enum].positions[i][2]+agents[enum].ps[1][n])
@@ -102,24 +112,28 @@ def sim_animation(limits, walls, trees, agents, past=True, start=True):
                 vis_domain[enum][n].theta1 = vis_o1
                 vis_domain[enum][n].theta2 = vis_o2
                 vis_domain[enum][n]._recompute_path()
-                # signals
-                #vis_domain[enum][n].set_visible = False
+                # aud signals
                 vis_val = agents[enum].states[i][n]
                 if vis_val > 0:
-                    print(vis_val)
-                    vis_domain[enum][n].set_visible = True
+                    vis_domain[enum][n].set_visible(True)
+                else:
+                    vis_domain[enum][n].set_visible(False)
+
             # check energy values
             energy = agents[enum].states[i][5]
-            if energy <= 25 and energy > 0:
+            if energy > 25:
+                agent_obj.set_color("blue")
+                agent_obj.fill = True
+            if 0 < energy <= 25:
                 agent_obj.set_color("red")
             if energy <= 0:
                 agent_obj.set_color("black")
                 agent_obj.fill = False
-                olf_domain[enum].set_visible =False
-                aud_domain[enum][0].set_visible = False
-                aud_domain[enum][1].set_visible = False
-                vis_domain[enum][0].set_visible = False
-                vis_domain[enum][1].set_visible = False
+                olf_domain[enum].set_visible(False)
+                # aud_domain[enum][0].set_visible = False
+                # aud_domain[enum][1].set_visible = False
+                vis_domain[enum][0].set_visible(False)
+                vis_domain[enum][1].set_visible(False)
             all_aud = [aud for audx in aud_domain for aud in audx]
             all_vis = [vis for visx in vis_domain for vis in visx]
 
@@ -128,11 +142,15 @@ def sim_animation(limits, walls, trees, agents, past=True, start=True):
     anim = animation.FuncAnimation(fig, animate,
                                         init_func=init,
                                         frames=100,
-                                        blit=True)
+                                        interval=200,
+                                        repeat=True,
+                                        blit=False)
     plt.show()
 
 
-
+# run
+d1,d2,d3,d4 = simulation.world_simulation()
+sim_animation(d1,d2,d3,d4)
 
 
 
