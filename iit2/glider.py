773cc50c186b7e1873ec169b9492d3c6b336f36b
Fernando Rodríguez Vergara <ferodriguezv@gmail.com>
2022-09-07 07:44:45 +0100

    mxs or pws?

--- iit2/glider.py
+++ iit2/glider.py
@@ -2,6 +2,7 @@
 import numpy as np
 from aux import *
 from pyemd import emd
+from itertools import combinations,chain
 
 class Glider:
     def __init__(self):
@@ -11,21 +12,19 @@ class Glider:
         self.sts = np.zeros((16,3,5,5))
         # transition matrix in empty env
         self.tm = np.zeros((16,16)).astype(int)
-        self.mk_sts()
-        # powerset of purviews
-        self.pws = bin_matrix(5,nans=True)
-        # cfgs where cell vals == 0/1
-        self.cvw = np.zeros((9,2,16))
-        # cells in purview for every 3x3 center cell
-        self.ces_pws = np.zeros((9,5,5))
-        self.mk_cvs()
+        # cell values (1st mechanisms) in past & fut sts (cell, cfg vals)
+        self.mxp = np.zeros((9,16))
+        self.mxf = np.zeros((9,16))
+        # active cells in each active cell domain (purview), for every glider cfg (gl st,pws,cfgs)
+        self.pws = np.zeros((16,31,16))
+        self.mk_glider()
         # cause and effect repertoires (cfs,pws,cells,dists)
         self.cxs = np.zeros((16,32,2,2,2,2,2,16))
         self.mk_cxs()
         self.exs = np.zeros((16,32,2,2,2,2,2,16))
-        self.mk_exs()
+        #self.mk_exs()
 
-    def mk_sts(self):
+    def mk_glider(self):
         # glider base sts (SE)
         gi = np.array([[0,0,1],[1,0,1],[0,1,1]])
         gx = np.array([[1,0,0],[0,1,1],[1,1,0]])
@@ -48,22 +47,44 @@ class Glider:
             self.sts[4*r+3,0] = np.roll(self.sts[4*r+3,0],di,axis=0)
             # txs, only for the empty case
             self.tm[4*r:4*(r+1),4*r:4*(r+1)] = np.roll(np.diag((1,1,1,1)),1,axis=1)
+        # for easier access, txs past and future values for 3x3 cells
+        # no need for bin 0/1 distinction: 0 means not a part of the system
+        self.mxp = self.sts[:,0,1:-1,1:-1].reshape(16,9).T
+        self.mxf = self.sts[:,2,1:-1,1:-1].reshape(16,9).T
+        # active cells for each glider cfg (5)
+        cfgs = self.sts[:,1,1:-1,1:-1].reshape(16,9)
+        # for composition of higher order pws (AB,ABC,ABCD,etc) (32 pws)
+        xis = list(chain.from_iterable([list(combinations(np.arange(5),i)) for i in range(2,6)]))
+        # first order purviews for each gl state/cfg
+        for cfg in range(16):
+            # active cells in cfg (so, that they can have a purview) (5)
+            cfg_pws = cfgs[cfg].nonzero()[0]
+            # gl states in which these cells are active (valid pws) from the 16 possible sts
+            self.pws[cfg][:5] = cfgs[:,cfg_pws].T
+            # higher order purviews
+            for xe,xi in enumerate(xis):
+                # combination of the 5 first order pws
+                self.pws[cfg][xe+5] = np.product(self.pws[cfg][np.array(xi)],axis=0)
 
-    def mst(self):
+    def glst(self):
+        # simple return glider state as 2d rep
         return self.sts[self.st,1]
 
-    def mk_cvs(self):
-        # cfgs where 3x3 center cells == 1 or 0
-        self.cvw[:,1,:] = self.sts[:,1,1:-1,1:-1].reshape(16,9).T
-        self.cvw[:,0,:] = np.abs(self.cvw[:,1,:]-1)
-        # pw for all 3x3 cells. sliding window
-        for i in range(1,4):
-            for j in range(1,4):
-                w = np.zeros((5,5)).astype(int)
-                w[i-1:i+2,j-1:j+2] = 1
-                self.ces_pws[3*(i-1)+j-1] = w
-
     def mk_cxs(self):
+        # first order causes
+        for cfg in range(16):
+            # glider cfg
+            gl = self.sts[cfg,1,1:-1,1:-1]
+            # first order active mechanisms (5 cells) for this cfg
+            mxs = self.mxp[gl.flatten().nonzero()[0]]
+            for me,mx in enumerate(mxs):
+                # gl past sts that could have led to cell/mx activation (=1)
+                glp = np.matmul(self.tm,mx)
+                # for every purview (A,B,C,D,E,AB,...,ABCDE) (32)
+
+
+                import pdb; pdb.set_trace()
+
         # first order causes
         for cfg in range(16):
             # active cells forming the glider
@@ -115,9 +136,6 @@ class Glider:
                         self.cxs[cfg,pwi,c0,c1,c2,c3,c4] = cx
 
 
-        # for all at the same time
-        # (self.sts[cfg,1]*self.ces_pws[ces])[:,1:-1,1:-1].reshape(5,9)
-        #self.pws*ces
 
 
 
@@ -161,11 +179,22 @@ class Glider:
 
 
 
-
-
-
-
-
+# pws (domain) for all 3x3 cells. sliding window
+# ces_pws = np.zeros((9,5,5))
+# for i in range(1,4):
+#     for j in range(1,4):
+#         w = np.zeros((5,5)).astype(int)
+#         w[i-1:i+2,j-1:j+2] = 1
+#         ces_pws[3*(i-1)+j-1] = w
+# # reduce to 3x3 (empty env, so no active cells outside)
+# ces_pws = ces_pws[:,1:-1,1:-1]
+# active cells for every glider cfg
+# cfgs = self.sts[:,1,1:-1,1:-1]
+# active cells in each active cell purview for every glider cfg
+# for cfg in range(16):
+    # pw : glider cfg * cells purview * only active cells in that cfg
+    # cfg_pws = cfgs.reshape(16,9)[cfg] * ces_pws.reshape(9,9) * np.array(self.sts[cfg,1,1:-1,1:-1].flatten()).reshape(9,1)
+    # self.pws[cfg][:5] = cfg_pws[~np.all(cfg_pws==0,axis=1)]
 
 
 
